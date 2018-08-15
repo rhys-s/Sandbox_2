8a1fb1b972b08dbb4b5cb8151a41a0544b0d7f4f
rhys.s <rhyss074@gmail.com>
2018-08-15 19:30:00 +1000

    Added password checker

--- Prac_03/password_entry.py
+++ Prac_03/password_entry.py
@@ -1,1 +1,8 @@
 """Rhys Selwyn"""
+pw = str(input('Please put word'))
+if len(pw) >= 6 or len(pw) <= 3:
+    print('Wrong passowrd')
+    pw = str(input('Please put word'))
+elif len(pw) < 6 and len(pw) > 3:
+    for i in range (len(pw)):
+        print('*',end='')
